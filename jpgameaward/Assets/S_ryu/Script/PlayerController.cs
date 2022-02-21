e457f16cf20fe2a93df4b62a0ce97f93b621f71e
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2022-02-21 14:21:44 +0900

    キャラクターの移動２

--- jpgameaward/Assets/S_ryu/Script/PlayerController.cs
+++ jpgameaward/Assets/S_ryu/Script/PlayerController.cs
@@ -4,33 +4,17 @@ using UnityEngine;
 
 public class CapsuleMove : MonoBehaviour
 {
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-
-    private Vector3 moveDirection = Vector3.zero;
-    private CharacterController controller;
+    Vector3 move;
+    public float speed;
+    public Rigidbody rb;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (controller.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection = moveDirection * speed;
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
-        }
-
-        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
-        controller.Move(moveDirection * Time.deltaTime);
     }
 }
