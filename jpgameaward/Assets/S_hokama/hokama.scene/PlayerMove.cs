48b4ba4b6b9fcd5efb63df6ca0fa41e9b6c38ca3
KBC12006024\kbc12006024 <kaito-hokama-20h@stu.kbc.ac.jp>
2022-02-24 15:05:28 +0900

    コントローラー移動実装

--- jpgameaward/Assets/S_hokama/hokama.scene/PlayerMove.cs
+++ jpgameaward/Assets/S_hokama/hokama.scene/PlayerMove.cs
@@ -1,69 +1,179 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
+
+//public class PlayerMove : MonoBehaviour
+//{
+//    [SerializeField] public bool inJumping = false;
+//    private bool Ground; // 地面に着地しているか判定する変数
+//    public float Jumppower; // ジャンプ力
+//    Rigidbody rb;
+//    SimpleAnimation simpleAnimation;
+//    ;
+
+
+//    void Start()
+//    {
+//        rb = this.GetComponent<Rigidbody>();
+//        rb.constraints = RigidbodyConstraints.FreezeRotation;
+//        simpleAnimation = this.GetComponent<SimpleAnimation>();
+//    }
+
+//    void Update()
+//    {
+
+//        float dx = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+//        transform.position = new Vector3(transform.position.x + dx, 0.5f);
+
+//        if (dx == 0)
+//        {
+//            simpleAnimation.Play("Default");
+//        }
+//        else
+//        {
+//            speed = 6f;
+//            simpleAnimation.CrossFade("Run", 0.1f);
+
+//            if (Input.GetButton("RT")){
+//            speed = 12f;
+//            simpleAnimation.CrossFade("Sprint", 0.1f);
+//            }
+//        }
+        
+//        Transform myTransform = this.transform;
+//        Vector3 localAngle = myTransform.localEulerAngles;
+//        if (dx < 0)
+//        {
+//            localAngle.y = 270.0f;
+//            myTransform.localEulerAngles = localAngle;
+//        }
+//        else
+//        {
+//            localAngle.y = 90.0f;
+//            myTransform.localEulerAngles = localAngle;
+//        }
+
+//    }
+
+//}
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
-    public Slider slider;
-    Rigidbody rb;
+    //プレイヤーフラグ
+    [SerializeField] public bool inJumping = false;
+    private Rigidbody rb; // Rigidbodyを使うための変数
+    private bool Ground; // 地面に着地しているか判定する変数
+    public float Jumppower; // ジャンプ力
+    float speed = 18f;
+    //SimpleAnimation変数
     SimpleAnimation simpleAnimation;
-    float speed = 6f;
-
 
     void Start()
     {
-        slider.value = 3;
-        rb = this.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();//  rbにRigidbodyを代入
+        //キャラクターが回転してしまわないように回転方向を固定する
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        //キャラクターのSimpleAnimationを取得
         simpleAnimation = this.GetComponent<SimpleAnimation>();
+        
     }
 
     void Update()
     {
-
+        //横移動とダッシュ
+        
         float dx = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        transform.position = new Vector3(transform.position.x + dx, 0.5f);
+        transform.position = new Vector3(transform.position.x + dx, transform.position.y);
 
-        if (dx == 0)
+        if (Input.GetButton("X"))
         {
-            simpleAnimation.Play("Default");
+            //Debug.Log("このボタン");
+            // joystick button 0 A
+            // joystick button 1 B
+            // joystick button 2 X
+            // joystick button 3 Y
+            // joystick button 4 LB
+            // joystick button 5 RB
+            // joystick button 6 BACK
+            // joystick button 7 START
+            // joystick button 8 L3
+            // joystick button 9 R3
+        }
+
+        //スペースでジャンプ
+        if (Input.GetButton("A"))//  もし、スペースキーがおされたなら、
+        {
+            if (Ground == true)//  もし、Groundedがtrueなら、
+            {
+                Ground = false;
+                inJumping = true;
+                rb.AddForce(Vector3.up * Jumppower);//  上にJumpPower分力をかける
+            }
+        }
+
+
+        //アニメーションの再生
+        if (dx != 0)
+        {
+            if (inJumping == true) //ジャンプ中のとき
+            {
+                simpleAnimation.CrossFade("Jump", 0.1f);        //ジャンプアニメーションを再生
+            }
+            //ダッシュキー押下時
+            else if (Input.GetButton("X"))
+            {
+                simpleAnimation.CrossFade("Sprint", 0.1f);      //ダッシュアニメーションを再生
+                speed = 18f;
+                if (Input.GetButton("B"))
+                {
+                    simpleAnimation.CrossFade("attack", 0.1f);
+                }
+            }
+            else
+            {
+                simpleAnimation.CrossFade("Run", 0.1f);         //普通移動アニメーションを再生
+                speed = 12f;
+                if (Input.GetButton("B"))
+                {
+                    simpleAnimation.CrossFade("attack", 0.1f);
+                }
+            }
+        }
+
+        else if (inJumping == true) //ジャンプ中のとき
+        {
+            simpleAnimation.CrossFade("Jump", 0.1f);        //ジャンプアニメーションを再生
         }
         else
+        //アタックテスト
+        if (Input.GetButton("B")) 
         {
-            simpleAnimation.CrossFade("Run", 0.1f);
+            simpleAnimation.CrossFade("attack", 0.1f);
         }
-        Transform myTransform = this.transform;
-        Vector3 localAngle = myTransform.localEulerAngles;
-        if (dx < 0)
-        {
-            localAngle.y = 270.0f;
-            myTransform.localEulerAngles = localAngle;
-        }
+
         else
         {
-            localAngle.y = 90.0f;
-            myTransform.localEulerAngles = localAngle;
+            simpleAnimation.Play("Default");        //デフォルトアニメーションを再生
         }
         
+        
+        
+
     }
 
-    void OnCollisionEnter(Collision col)
+    //地面との判定
+    void OnCollisionEnter(Collision other)//  地面に触れた時の処理
     {
-        if (col.gameObject.name == "PointDown")
+        if (other.gameObject.tag == "Ground")//  もしGroundというタグがついたオブジェクトに触れたら、
         {
-            if (slider.value > 0)
-            {
-                slider.value--;
-            }
-        }
-        if (col.gameObject.name == "PointUp")
-        {
-            if (slider.value < 4)
-            {
-                slider.value++;
-            }
+            Ground = true;//  Groundedをtrueにする
+            inJumping = false;
         }
     }
 }
-
