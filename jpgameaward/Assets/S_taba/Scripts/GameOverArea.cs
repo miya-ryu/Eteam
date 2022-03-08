636d5f2843ae63d156a1d1a9fd21a5d4b4cc147f
KBC12006188\rikut <rikuto-taba-20h@stu.kbc.ac.jp>
2022-03-08 14:37:45 +0900

    橋、坂の修正、リスポーンの実装

---
+++ jpgameaward/Assets/S_taba/Scripts/GameOverArea.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverArea : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //RigidBodyを取得
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GameOverArea"))
+        {
+            Debug.Log("ステージから落ちた。");
+            this.transform.position = new Vector3(16.96f, 11.07f, -0.73f);
+
+            //rb.isKinematic = true;
+            //rb.velocity = Vector3.zero;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    //private IEnumerator stop()
+    //{
+    //    //2秒待つ
+    //    yield return new WaitForSecondsRealtime(2);
+
+    //    rb.isKinematic = false;
+    //}
+}
