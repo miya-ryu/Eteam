d59ce9f9713d8e63f11cf233b594596d3d0f72ca
KBC12006188\rikut <rikuto-taba-20h@stu.kbc.ac.jp>
2022-03-29 11:53:41 +0900

    溜めエフェクト

---
+++ jpgameaward/Assets/S_taba/Scripts/ParticlePlaying.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlaying : MonoBehaviour
+{
+    private bool isPlaying;
+    [SerializeField] ParticleSystem Chargeparticle;
+    [SerializeField] ParticleSystem Circleparticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Chargeparticle.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        Circleparticle.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+
+        //Invoke("DestroyParticle", 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("B") || Input.GetKey("space"))
+        {
+            Chargeparticle.Play(true);
+            Circleparticle.Play(true);
+
+            Circleparticle.gameObject.SetActive(true);
+        }
+        else
+        {
+            Chargeparticle.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            Circleparticle.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+
+            Circleparticle.gameObject.SetActive(false) ;
+
+        }
+    }
+    //void DestroyParticle()
+    //{
+    //    Destroy(Circleparticle);
+    //}
+
+    //public void Switch()
+    //{
+    //    if (isPlaying)
+    //    {
+    //        particle.Play(true);
+    //    }
+    //    else
+    //    {
+    //        particle.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+    //    }
+    //    isPlaying = !isPlaying;
+    //}
+}
