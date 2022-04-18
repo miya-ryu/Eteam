d60e5f8999452fd59f2c548c1bc9f6d0cf762b84
KBC12006188\rikut <rikuto-taba-20h@stu.kbc.ac.jp>
2022-04-18 09:26:51 +0900

    溜めエフェクトの変更

--- jpgameaward/Assets/S_taba/Scripts/Rush_Ray.cs
+++ jpgameaward/Assets/S_taba/Scripts/Rush_Ray.cs
@@ -11,8 +11,23 @@ public class Rush_Ray : MonoBehaviour
     RaycastHit hit;
     Vector3 direction;  //Rayを飛ばす方向
     float distance = 10;//Rayを飛ばす距離
-    Vector3 pos;        //移動の座標格納用
-    float step;         //移動用
+
+    //test2
+    [SerializeField] GameObject player;
+
+    public float speed;
+    public GameObject target;
+
+    private void Start()
+    {
+        speed = 1f;
+    }
+
+    //void Update()
+    //{
+    //    //自分の位置、ターゲット、速度
+    //    transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+    //}
 
     private void OnTriggerStay(Collider other)
     {
@@ -27,26 +42,41 @@ public class Rush_Ray : MonoBehaviour
         //if(Physics.Raycast(ray.origin,ray.direction * distance,out hit))
         hit = Physics.RaycastAll(ray).First();
         {
-            pos = hit.point;
-            pos.y = 1f;
+
             if (hit.collider.CompareTag("Enemy"))
             {
                 Debug.Log("敵発見");
+                Debug.Log(hit.point);//デバッグログにヒットした場所を出す
 
-                if(Input.GetKeyUp(KeyCode.Space))
+                if (Input.GetButtonUp("B") || Input.GetKeyUp(KeyCode.Space))
                 {
-                    transform.position = Vector3.MoveTowards(transform.position, pos, step);
+                    //transform.position = Vector3.MoveTowards(transform.position, pos, step);//test1
+
+                    //this.transform.position = hit.point;//test2
+
+                    Vector3 hitpoint = hit.point;
+
+                    //this.transform.position =
+                    //    new Vector3(this.transform.position.x, hitpoint.y, hitpoint.z);//test2 zだけ
+
+                    transform.position = Vector3.MoveTowards(transform.position, hitpoint, speed);
+                    Invoke("Moves", 0f);
                 }
 
             }
-            //else if (hit.collider.CompareTag("Ground"))
-            //{
-            //    Debug.Log("");
-            //}
             else
             {
                 Debug.Log("何もない");
             }
+        }
+    }
+    IEnumerator MoveUp()
+    {
+        while (direction.y < 3.0f)
+        {
+            direction = transform.position;
+            transform.Translate(hitpoint.x, 0.02f, 0);
+            yield return new WaitForSeconds(0.01f);
         }
     }
 }
