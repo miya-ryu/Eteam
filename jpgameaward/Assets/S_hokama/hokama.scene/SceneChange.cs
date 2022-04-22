210b4e71de07cef2f19a2fe54e174b1f8a819661
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2022-04-22 10:36:13 +0900

    no message

--- jpgameaward/Assets/S_hokama/hokama.scene/SceneChange.cs
+++ jpgameaward/Assets/S_hokama/hokama.scene/SceneChange.cs
@@ -7,27 +7,15 @@ public class SceneChange : MonoBehaviour
     int pause=GameManager.pause;
     void Update()
     {
-<<<<<<< HEAD
-        pause = GameManager.pause;
-        
-        if (SceneManager.GetActiveScene().name == "TitleScene" && Input.GetButton("start")) 　//シーンがTitleSceneの時startボタンが押されたら
-        {
-            SceneManager.LoadScene("SampleScene 2");
-        }
-        if (SceneManager.GetActiveScene().name == "ResultScene" && Input.GetButton("start"))  //シーンがResultSceneの時startボタンが押されたら
-=======
-
         if (SceneManager.GetActiveScene().name == "TitleScene" && Input.GetButton("start")) 　//シーンがTitleSceneの時startボタンが押されたら
         {
             SceneManager.LoadScene("StageSelect");
-            //SceneManager.LoadScene("1Scene");
         }
         if (SceneManager.GetActiveScene().name == "ClearScene" && Input.GetButton("start")) 　//シーンがClearSceneの時startボタンが押されたら
->>>>>>> origin/matayoshi10
         {
             SceneManager.LoadScene("TitleScene");
-            //SceneManager.LoadScene("1Scene");
         }
+
         if (SceneManager.GetActiveScene().name == "SampleScene 2" && pause == 1)
         {
             if (Input.GetButton("A"))
@@ -41,21 +29,5 @@ public class SceneChange : MonoBehaviour
             }
 
         }
-
-        //if (SceneManager.GetActiveScene().name == "TitleScene" && Input.GetButton("start")) 　//シーンがTitleSceneの時startボタンが押されたら
-        //{
-        //    SceneManager.LoadScene("SampleScene 2");
-        //    //SceneManager.LoadScene("1Scene");
-        //}
-        //if (SceneManager.GetActiveScene().name == "SampleScene 2" && Input.GetButton("X"))　　　 //シーンが1Sceneの時startボタンが押されたら
-        //{
-        //    SceneManager.LoadScene("ResultScene");
-        //}
-
-        //if (SceneManager.GetActiveScene().name == "ResultScene" && Input.GetButton("start"))  //シーンがResultSceneの時startボタンが押されたら
-        //{
-        //    SceneManager.LoadScene("TitleScene");
-        //}
-
     }
 }
