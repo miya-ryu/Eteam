f31c4bbaee61e81a3e699031306f694263b4635c
KBC12006188\rikut <rikuto-taba-20h@stu.kbc.ac.jp>
2022-04-25 09:52:24 +0900

    回復エフェクト

---
+++ jpgameaward/Assets/S_taba/Scripts/Kaifuku_Effect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kaifuku_Effect : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem kakifuku_Effect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("回復した");
+            //
+            ParticleSystem newParticle = Instantiate(kakifuku_Effect);
+            //
+            newParticle.transform.position = this.transform.position;
+            //
+            newParticle.transform.parent = collision.gameObject.transform;
+            //
+            newParticle.Play();
+        }
+    }
+}
