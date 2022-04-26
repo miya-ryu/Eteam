91bb12a95c1bce03d3bdd0a0beda37f49c7d5503
KBC12006077\kbc12006077 <hiroto-matayoshi-20h@stu.kbc.ac.jp>
2022-04-26 14:07:29 +0900

    ステージ解放

--- jpgameaward/Assets/matayoshi/Script/StageClear.cs
+++ jpgameaward/Assets/matayoshi/Script/StageClear.cs
@@ -5,19 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class StageClear : MonoBehaviour
 {
-    [SerializeField] public int stageNum; //このボスが登場するステージ番号をインスペクターに指定
+    [SerializeField] public int stageNum;
 
     void Update()
     {
 
     }
 
-    public void StageOpen()
-    {
-        PlayerPrefs.SetInt("stageNumber", stageNum);//PlayerPrefsでクリアしたステージ番号をセット
-    }
     public void OnDestroy()
     {
         SceneManager.LoadScene("ClearScene");
+        StageOpen();
+    }
+
+    public void StageOpen()
+    {
+        PlayerPrefs.SetInt("stageNumber", stageNum);//PlayerPrefsでクリアしたステージ番号をセット
     }
 }
---
+++ jpgameaward/Assets/matayoshi/Script/StageSelect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelect : MonoBehaviour
+{
+    private int stageNumber;
+    public Button Stage2;
+    public Text Stage2Text;
+
+    void Start()
+    {
+        stageNumber = PlayerPrefs.GetInt("stageNumber");
+        Stage2.interactable = false;
+        PlayerPrefs.DeleteAll();
+    }
+
+    void Update()
+    {
+        if (stageNumber >= 2)
+        {
+            Stage2.interactable = true;
+            Stage2Text.text = "STAGE2"; //ステージ2の名前
+        }
+    }
+}
