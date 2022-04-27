c837599393cd0674e6b77767ab7fb1d7c60ef55f
KBC12006024\kbc12006024 <kaito-hokama-20h@stu.kbc.ac.jp>
2022-04-27 13:23:43 +0900

    リザルト画面作成（残り時間と敵の倒した数を表示　BGMは未実装）

--- jpgameaward/Assets/S_hokama/Attckwall.cs
+++ jpgameaward/Assets/S_hokama/Attckwall.cs
@@ -7,19 +7,28 @@ public class Attckwall : MonoBehaviour
     bool CAttackflg = PlayerMove2.ChargeAttack;
     public BoxCollider col;
 
+    GameObject[] enemyBox;
+    public static int AllEnemy;
+    public static int Enemycount;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        enemyBox = GameObject.FindGameObjectsWithTag("Enemy");
+        AllEnemy = enemyBox.Length;
+
         col.enabled = false;
     }
     void Update()
     {
+        enemyBox = GameObject.FindGameObjectsWithTag("Enemy");
+        Enemycount = enemyBox.Length;
         CAttackflg = PlayerMove2.ChargeAttack;
         if (CAttackflg == true)
         {
             col.enabled = true;
         }
-
         if (CAttackflg == false)
         {
             col.enabled = false;
