e39628fc346e58cc5043bfba670cc08b9a028be7
KBC12006188\rikut <rikuto-taba-20h@stu.kbc.ac.jp>
2022-05-10 15:19:27 +0900

    a

---
+++ jpgameaward/Assets/S_taba/Scripts/stageNum.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class stageNum : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize()
+    {
+        PlayerPrefs.DeleteKey("stageNumber");
+    }
+}
