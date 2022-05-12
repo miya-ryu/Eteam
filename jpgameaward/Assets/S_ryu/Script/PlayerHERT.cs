5efae567752e415780574bc21c6bbebf5a838596
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2022-05-12 13:34:14 +0900

    ｚ

--- jpgameaward/Assets/S_ryu/Script/PlayerHERT.cs
+++ jpgameaward/Assets/S_ryu/Script/PlayerHERT.cs
@@ -5,43 +5,37 @@ using UnityEngine;
 public class PlayerHERT : MonoBehaviour
 {
     public GameObject heart, heart1, heart2;
-    float lifecount =  3;
+    float lifecount = 3;
     const float MAX = 3;
-
     float count;
     bool CAttack = PlayerMove2.ChargeAttack;
-
     [SerializeField] GameObject GameOver_flg;
-
     //SoundScript の Damage 関数を取得
     public SoundScript DamageClip;
     //SoundScript の Helse 関数を取得
     public SoundScript HelseClip;
-
+    [SerializeField] private ParticleSystem kakifuku_Effect;
+    [SerializeField] private GameObject Player;
     void Start()
     {
         count = 5f;
     }
-
     void Update()
     {
         Damage();
         lifeup();
-
         count += Time.deltaTime;
-
         //PlayerMove2 スクリプトの ChargeAttack を代入
         CAttack = PlayerMove2.ChargeAttack;
     }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Konbou")
         {
-            if(count > 2)
+            if (count > 2)
             {
                 //攻撃をしていなければ
-                if(CAttack == false)
+                if (CAttack == false)
                 {
                     lifecount--;
                     //音を鳴らす
@@ -49,13 +43,11 @@ public class PlayerHERT : MonoBehaviour
                     count = 0;
                 }
             }
-
-            if(lifecount < 0)
+            if (lifecount < 0)
             {
                 lifecount = 0;
             }
         }
-
         if (other.gameObject.tag == "Helse")
         {
             if (lifecount == 3)
@@ -63,6 +55,13 @@ public class PlayerHERT : MonoBehaviour
             }
             else
             {
+                ParticleSystem newParticle = Instantiate(kakifuku_Effect);
+                //
+                newParticle.transform.position = Player.transform.position;
+                //
+                newParticle.transform.parent = Player.gameObject.transform;
+                //
+                newParticle.Play();
                 lifecount++;
                 //音を鳴らす
                 HelseClip.Helse();
@@ -71,7 +70,6 @@ public class PlayerHERT : MonoBehaviour
             }
         }
     }
-
     void Damage()
     {
         if (lifecount == 2)
@@ -88,7 +86,6 @@ public class PlayerHERT : MonoBehaviour
             GameOver_flg.GetComponent<GameOver_flg>().enabled = true;
         }
     }
-
     void lifeup()
     {
         if (lifecount == 3 || heart == false)
@@ -100,5 +97,6 @@ public class PlayerHERT : MonoBehaviour
             heart1.SetActive(true);
         }
     }
+
 }
 
--- jpgameaward/Assets/S_taba/Scripts/KATANA_Effect2.cs
+++ jpgameaward/Assets/S_taba/Scripts/KATANA_Effect2.cs
@@ -6,28 +6,45 @@ public class KATANA_Effect2 : MonoBehaviour
 {
     TrailRenderer KATANA_TrailRenderer;
     //private TrailRenderer;
-
+    public int cntTrail = 0;    //溜め時間を管理する変数
+    const int MAXCNT = 60; //溜まりきる時間を格納する変数
+    [SerializeField] public ParticleSystem Blood = null;
     // Start is called before the first frame update
     void Start()
     {
         KATANA_TrailRenderer = GetComponent<TrailRenderer>();
-
         KATANA_TrailRenderer.enabled = false;
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("B"))
+        if (Input.GetButton("B") || Input.GetKey(KeyCode.Space))//Bボタンで長押しで溜めエフェクト
         {
-            KATANA_TrailRenderer.enabled = true;
-
-            Invoke("Delete_Effect", 0.8f);
+            cntTrail++;
+        }
+        else
+        {
+            if (cntTrail >= 60)
+            {
+                KATANA_TrailRenderer.enabled = true;
+                Invoke("Delete_Effect", 0.8f);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //攻撃時に敵と衝突したら
+        if (other.gameObject.tag == "Enemy")
+        {
+            // パーティクルシステムのインスタンスを生成する。
+            ParticleSystem blood = Instantiate(Blood);
+            blood.transform.position = this.transform.position;
+            blood.Play();
         }
     }
     void Delete_Effect()
     {
-
         KATANA_TrailRenderer.enabled = false;
+        cntTrail = 0;
     }
 }
